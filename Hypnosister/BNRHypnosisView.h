8f269041ec5ee5c9d90e215877f254950b87b8ee
Krzysztof Kula <krzychukula@gmail.com>
2014-07-28 09:54:51 +0200

    page 91 new view

---
+++ Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Krzysztof Kula on 28.07.2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
